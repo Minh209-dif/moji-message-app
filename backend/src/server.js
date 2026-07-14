31264d90db5f4a7fbc31fe1cb46d026fca9dbf6e
Minh209-dif <ngonhatminh672@gmail.com>
2026-07-14 16:25:29 +0700

    chore(backend): setup express server and dependencies

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './libs/db.js';
+import authRoute from './routes/authRoute.js';
+import userRoute from './routes/userRoute.js';
+import { protectedRoute } from './middlewares/authMiddleware.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -9,6 +14,16 @@ const PORT = process.env.PORT || 5001;
 
 //middlewares
 app.use(express.json());
+app.use(cookieParser());
+app.use(cors({origin: process.env.CLIENT_URL, credentials: true}));
+
+// Public route
+app.use('/api/auth', authRoute);
+
+// Private route
+app.use(protectedRoute); // Apply the protectedRoute middleware to all routes below this line
+app.use('/api/users', userRoute);
+
 connectDB().then(() => {
     app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
