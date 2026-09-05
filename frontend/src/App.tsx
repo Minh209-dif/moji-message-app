9b8744aeba9c43dc8fcfd3f265f0cd9b13d68a88
Minh209-dif <ngonhatminh672@gmail.com>
2026-09-05 17:50:25 +0700

    feat(frontend): add chat and messaging state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,30 @@ import { BrowserRouter, Route, Routes } from 'react-router';
 import SignInPage from './pages/SignInPage';
 import SignUpPage from './pages/SignUpPage';
 import ChatAppPage from './pages/ChatAppPage';
+import ProtectedRoute from './components/auth/ProtectedRoute';
 import { Toaster } from 'sonner';
+import { useThemeStore } from './stores/useThemeStore';
+import { useEffect } from 'react';
+import { useAuthStore } from './stores/useAuthStore';
+import { useSocketStore } from './stores/useSocketStore';
 
 function App() {
+  const { isDark, setTheme } = useThemeStore();
+  const {accessToken} = useAuthStore();
+  const {connectSocket, disconnectSocket} = useSocketStore();
+
+  useEffect(() => {
+    setTheme(isDark);
+  }, [isDark]);
+
+  useEffect(() => {
+    if(accessToken) {
+      connectSocket();
+    }
+
+    return () => disconnectSocket();
+  }, [accessToken]);
+  
   return (
     <>
       <Toaster richColors />
@@ -15,7 +36,9 @@ function App() {
           <Route path="/signup" element={<SignUpPage />} />
 
           {/* Protected route */}
-          <Route path="/" element={<ChatAppPage />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="/" element={<ChatAppPage />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
--- frontend/src/pages/ChatAppPage.tsx
+++ frontend/src/pages/ChatAppPage.tsx
@@ -1,5 +1,16 @@
+import { AppSidebar } from "@/components/sidebar/app-sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import ChatWindowLayout from "@/components/chat/ChatWindowLayout";
+
 const ChatAppPage = () => {
-    return <div>Chat App Page</div>;
+    return (
+        <SidebarProvider>
+            <AppSidebar/>
+            <div className="flex h-screen w-full p-2">
+                <ChatWindowLayout />
+            </div>
+        </SidebarProvider>
+    );
 };
 
 export default ChatAppPage;
